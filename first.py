3e7e0d2d82efa77516423358ece5c5e435ce829f
Badhan2440 <paulbadhan7109@gmail.com>
2022-03-14 16:03:06 +0600

    second commit

--- first.py
+++ first.py
@@ -1,9 +1,20 @@
+from pyexpat import model
 import pandas
 data= pandas.read_csv("iphone_price.csv")
 print(data.head()) 
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-plt.scatter(data['version'],data['price'])
-plt.bar(data['version'],data['price'])
-plt.show()
+# plt.scatter(data['version'],data['price'])
+# plt.bar(data['version'],data['price'])
+# plt.show()
+
+
+
+
+from sklearn.linear_model import LinearRegression
+
+model= LinearRegression()
+model.fit(data[['version']], data[['price']])
+
+print(model.predict([[20]]))
